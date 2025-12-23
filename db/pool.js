fb632beb125ee71d2dc42a4fda32e5c29c235a71
SoloGudLyf <solomonteiko0@gmail.com>
2025-12-23 22:08:23 +0000

    Use appropriate db connection url in dev or prod

--- db/pool.js
+++ db/pool.js
@@ -1,7 +1,6 @@
 import { Pool } from "pg";
 import "dotenv/config";
 
-// Again, this should be read from an environment variable
 export default new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL || process.env.DATABASE_PUBLIC_URL,
 });
