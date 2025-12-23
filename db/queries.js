4b528f4adebdd304136ff765b39cb2a7f3bdf96f
SoloGudLyf <solomonteiko0@gmail.com>
2025-12-23 09:39:47 +0000

    fix: Fix message insertion into db
    Insert author and message obtained from user into db

---
+++ db/queries.js
@@ -0,0 +1,18 @@
+import pool from "./pool.js";
+
+async function getAllUsernames() {
+  const { rows } = await pool.query("SELECT * FROM messages");
+  return rows;
+}
+
+async function insertUsername(author, username) {
+  await pool.query("INSERT INTO messages (author,message) VALUES ($1,$2)", [
+    author,
+    username,
+  ]);
+}
+
+export default {
+  getAllUsernames,
+  insertUsername,
+};
